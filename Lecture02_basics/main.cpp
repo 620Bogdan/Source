6882280f6b81deacd6c5bae0a3fbf5373e6ca178
620Bogdan <dizelgann@gmail.com>
2025-10-03 14:59:00 +0200

    ADD: lc2

--- Lecture02_basics/main.cpp
+++ Lecture02_basics/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>//подключение math для использования функции pow
+#include <cmath>
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -8,11 +8,10 @@ int main() {
 	cin >> a >> b;
 	c = pow(((a * a) + (b * b)), 0.5);
 	s = (a + b + c);
-	cout << "Гипотинуза: " << c << " float" << endl;
-	cout << "Периметр: " << s << " float" << endl;
-	cout << "int " << sizeof(int) * 8 << "бита, " << numeric_limits<int>::min() << " " << numeric_limits<int>::max() << endl;
-	cout << "float : " << sizeof(float) * 8 << "бита, " << numeric_limits<float>::lowest() << " " << numeric_limits<float>::max() << endl;
-	/*Завершение
-	программы*/
+	cout << "Р“РёРїРѕС‚РёРЅСѓР·Р°: " << c << " float" << endl;
+	cout << "РџР»РѕС‰Р°РґСЊ: " << s << " float" << endl;
+	cout << "int " << sizeof(int) * 8 << "Р±Р°Р№С‚, " << numeric_limits<int>::min() << " " << numeric_limits<int>::max() << endl;
+	cout << "float : " << sizeof(float) * 8 << "Р±Р°Р№С‚, " << numeric_limits<float>::lowest() << " " << numeric_limits<float>::max() << endl;
+
 	return 0;
 }
