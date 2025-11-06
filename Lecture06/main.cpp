bc79b943ba021b3285e0d0852e7008c1467e1547
620Bogdan <dizelgann@gmail.com>
2025-11-07 01:10:46 +0200

    //

---
+++ Lecture06/main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+
+int function(int a, int b) {
+    cout <<"исп. функция суммы 2х чисел" << endl;
+    return a + 2 * b + 3;
+}
+
+
+int function(int a, int b, int c){
+    cout << "исп. функция суммы 3х чисел" << endl;
+    int su1 = a + b;
+    int su2 = a + c;
+    int su3 = b + c;
+    int msu = su1;
+    if (su2 > msu) {
+        msu = su2;
+    }
+    if (su3 > msu) {
+        msu = su3;
+    }
+    return msu;
+}
+
+
+int main(){
+
+
+
+    int choice;
+    cout << "Выберите пункт(1 или 2):" << endl;
+    cout << "Введите номер пункта: ";
+    cin >> choice;
+
+
+    if (choice == 1) {
+        int a,b,c;
+        cout << "Введите три целых числа (если число = 0, то можно считать, что его нет):" << endl;
+        cout << "a = ";
+        cin >> a;
+        cout << "b = ";
+        cin >> b;
+        cout << "c = ";
+        cin >> c;
+
+
+        // Считаем количество ненулевых чисел
+        int count = 0;
+        if (a != 0) {
+            count++;
+        }
+        if (b != 0) {
+            count++;
+        }
+        if (c != 0) {
+            count++;
+        }
+        int result = 0;
+
+
+        if (count == 0) {
+            cout << "Ошибка: чисел нет!" << endl;
+            return 0;
+        }
+
+
+        else if (count == 1){
+            cout << "исп. функция для 1го числа" << endl;
+            if (a != 0){
+                result = a;
+            }
+            else if (b != 0){
+                result = b;
+            }
+            else {
+                result = c;
+            }
+        }
+
+
+        else if (count == 2){
+            int x[2], idx = 0;
+            if (a != 0) x[idx++] = a;
+            if (b != 0) x[idx++] = b;
+            if (c != 0) x[idx++] = c;
+
+            result = function(x[0], x[1]);
+        }
+        else if (count == 3){
+            result = function(a, b, c);
+        }
+
+        cout << "Полученный результат: " << result << endl;
+    }
+    else if (choice == 2) {
+        cout << "Пункт 2 не реализован (вариант для самостоятельного выполнения)" << endl;
+    }
+    else {
+        cout << "Неправильно введенное число, попробуйте ввести число заного." << endl;
+    }
+
+    return 0;
+}
